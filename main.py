dcd8ab0e42e2ebaf30ec14e52cbc84401bc2ad1a
Manu NALEPA <emmanuel.nalepa@pricemoov.com>
2020-11-21 16:57:33 +0100

    aa

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from math import cos, log, sin
 
 import ray
@@ -10,7 +11,13 @@ def function():
 
 
 if __name__ == "__main__":
-    ray.init(address="localhost:6379")
+    if "RAY_HEAD_SERVICE_HOST" not in os.environ or os.environ["RAY_HEAD_SERVICE_HOST"] == "":
+        raise ValueError(
+            "RAY_HEAD_SERVICE_HOST environment variable empty." "Is there a ray cluster running?"
+        )
+
+    redis_host = os.environ["RAY_HEAD_SERVICE_HOST"]
+    ray.init(address=redis_host + ":6379")
 
     futures = [function.remote() for _ in range(5)]
     ray.get(futures)
