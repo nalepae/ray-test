5a5cd7adb444a0636a5b19f30adcaca9cbc9366f
Manu NALEPA <emmanuel.nalepa@pricemoov.com>
2020-11-23 11:12:25 +0100

    dd

--- optim2.py
+++ optim2.py
@@ -5,13 +5,13 @@ from hyperopt.mongoexp import MongoTrials
 
 
 def to_optimize(x):
-    for i in range(1 * 10 ** 7):
+    for i in range(5 * 10 ** 7):
         sin(cos(log(i + 1)))
 
     return (x - 5) ** 2
 
 
-trials = MongoTrials("mongo://mongo:27017/hyperopt_db/jobs", exp_key="exp3")
+trials = MongoTrials("mongo://mongo:27017/hyperopt_db/jobs", exp_key="exp4")
 print("after trials")
 best = fmin(
     fn=to_optimize, space=hp.uniform("x", 0, 10), algo=tpe.suggest, max_evals=100, trials=trials
